7618a9ee2e0c91b9539d82b3bc6b549bf5f58e0f
mbaire <mbairemunge@gmail.com>
2021-08-03 23:13:21 +0300

    Modified Js file

--- JS/index.js
+++ JS/index.js
@@ -16,42 +16,42 @@ var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
 var pizzaNonVegTopping = ["Chicken", "Sausage", "Bacon", ];
 var pizzaVegTopping = ["Mushrooms", "Onions", "Capsicum", ];
 
-// order.prototype.cost = function() {
-//     if (this.size === pizzaSize[0]) {
-//         this.price += 500;
-//     } else if (this.size === pizzaSize[1]) {
-//         this.price += 700;
-//     } else if (this.size === pizzaSize[2]) {
-//         this.price += 900;
-//     }
+order.prototype.cost = function() {
+    if (this.size === pizzaSize[0]) {
+        this.price += 500;
+    } else if (this.size === pizzaSize[1]) {
+        this.price += 700;
+    } else if (this.size === pizzaSize[2]) {
+        this.price += 900;
+    }
 
-//     if (this.crust === pizzaCrust[0]) {
-//         this.price += 100;
-//     } else if (this.crust === pizzaCrust[1]) {
-//         this.price += 100;
-//     } else if (this.crust === pizzaCrust[2]) {
-//         this.price += 50;
-//     }
+    if (this.crust === pizzaCrust[0]) {
+        this.price += 100;
+    } else if (this.crust === pizzaCrust[1]) {
+        this.price += 100;
+    } else if (this.crust === pizzaCrust[2]) {
+        this.price += 50;
+    }
 
-//     if (this.nonVegTopping === pizzaNonVegTopping[0]) {
-//         this.price += 300;
-//     } else if (this.nonVegTopping === pizzaNonVegTopping[1]) {
-//         this.price += 300;
-//     } else if (this.nonVegTopping === pizzaNonVegTopping[2]) {
-//         this.price += 300;
-//     }
+    if (this.nonVegTopping === pizzaNonVegTopping[0]) {
+        this.price += 300;
+    } else if (this.nonVegTopping === pizzaNonVegTopping[1]) {
+        this.price += 300;
+    } else if (this.nonVegTopping === pizzaNonVegTopping[2]) {
+        this.price += 300;
+    }
 
 
-//     if (this.vegTopping === pizzaVegTopping[0]) {
-//         this.price += 200;
-//     } else if (this.vegTopping === pizzaVegTopping[1]) {
-//         this.price += 200;
-//     } else if (this.vegTopping === pizzaVegTopping[2]) {
-//         this.price += 200;
-//     }
+    if (this.vegTopping === pizzaVegTopping[0]) {
+        this.price += 200;
+    } else if (this.vegTopping === pizzaVegTopping[1]) {
+        this.price += 200;
+    } else if (this.vegTopping === pizzaVegTopping[2]) {
+        this.price += 200;
+    }
 
-//     return this.price;
-// }
+    return this.price;
+}
 
 
 order.prototype.totalCost = function() {
@@ -63,37 +63,37 @@ order.prototype.totalCost = function() {
 }
 
 
-$(document).ready(function() {
-    $("input#order1").click(function(event) {
-        event.preventDefault();
-        var flavour=$("select#flav").val();
-        var sizes = $("select#piz").val();
-        var crusts = $("select#crus").val();
-        var noVegToppings = $("select#topnoveg").val();
-        var vegToppings = $("select#topveg").val();
+// $(document).ready(function() {
+//     $("input#order1").click(function(event) {
+//         event.preventDefault();
+//         var flavour=$("select#flav").val();
+//         var sizes = $("select#piz").val();
+//         var crusts = $("select#crus").val();
+//         var noVegToppings = $("select#topnoveg").val();
+//         var vegToppings = $("select#topveg").val();
 
-        var newPizzaOrder = new order(flavour,sizes, crusts, noVegToppings, vegToppings);
-        newPizzaOrder.cost();
-        totalCosts.push(newPizzaOrder.price);
+//         var newPizzaOrder = new order(flavour,sizes, crusts, noVegToppings, vegToppings);
+//         newPizzaOrder.cost();
+//         totalCosts.push(newPizzaOrder.price);
 
-        $("#flv").text(flavour);
-        $("#sz").text(sizes);
-        $("#cr").text(crusts);
-        $("#nvgs").text(noVegToppings);
-        $("#vgs").text(vegToppings);
-        $("#tc").text(newPizzaOrder.totalCost());
+//         $("#flv").text(flavour);
+//         $("#sz").text(sizes);
+//         $("#cr").text(crusts);
+//         $("#nvgs").text(noVegToppings);
+//         $("#vgs").text(vegToppings);
+//         $("#tc").text(newPizzaOrder.totalCost());
 
 
-    });
+//     });
 
-    $("#order2").click(function() {
-        prompt("Please insert you name")
-        prompt("Please insert you location")
-        prompt("Please insert your addrress")
-        alert("You will be charged an extra 200 for delivery")
-        alert("Thank you for Your Purchase!Your order will be delivered to your location 🍕 🚚")
+//     $("#order2").click(function() {
+//         prompt("Please insert you name")
+//         prompt("Please insert you location")
+//         prompt("Please insert your addrress")
+//         alert("You will be charged an extra 200 for delivery")
+//         alert("Thank you for Your Purchase!Your order will be delivered to your location 🍕 🚚")
 
-    });
+//     });
 
 
     $(".picha").click(function() {
